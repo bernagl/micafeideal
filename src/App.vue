1fba626105e30e25d099caee8ff058a38b4d7f46
Luis García <luisgarcia@Luiss-MBP.lan>
2018-03-26 11:56:26 -0600

    add: vuex, bootstrap 4 grid and helpers, vue fixed header, at-ui

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <div>
+  <Header></Header>
+  <div class="container">
     <router-view/>
+  </div>
   </div>
 </template>
 
 <script>
+import Header from './components/header.vue'
 export default {
-  name: 'App'
+  name: 'App',
+  components: { Header }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import 'at-ui-style';
+@import 'bootstrap-4-grid';
 </style>
---
+++ src/components/header.vue
@@ -0,0 +1,22 @@
+<template>
+  <fixed-header :fixed.sync="isFixed" :threshold="100">
+    <div :class="{ 'is-fixed': isFixed }">
+      Your Content
+    </div>
+  </fixed-header>
+</template>
+
+<script>
+import FixedHeader from 'vue-fixed-header'
+
+export default {
+  components: {
+    FixedHeader
+  },
+  data() {
+    return {
+      isFixed: false
+    }
+  }
+}
+</script>
